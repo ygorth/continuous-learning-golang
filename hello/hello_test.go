162936d453fed83251566d98976a70b43f28a798
ygor <ygorth@gmail.com>
2021-08-11 02:16:13 -0400

    add subtests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
+// subtests around Hello()
 func TestHello(t *testing.T) {
-	got := Hello("Ygor")
-	want := "Hello, Ygor"
+	t.Run("saying hello to orcs", func(t *testing.T) {
+		got := Hello("Ygor")
+		want := "Hello, Ygor"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, Ygor' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Ygor"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
