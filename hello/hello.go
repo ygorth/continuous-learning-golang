f093616b58ab75550e8647a60f855087d096d597
Ygor Almeida <ygorth@gmail.com>
2024-11-08 23:20:13 -0500

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,36 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+// Hello returns a personalised greeting in a given language.
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Ygor"))
+    fmt.Println(Hello("world", ""))           // Default: Hello, world
+    fmt.Println(Hello("Control-C", spanish))       // Spanish: Hola, Ygor
+    fmt.Println(Hello("Marie", french))       // French: Bonjour, Marie
+    fmt.Println(Hello("", ""))                // Default: Hello, World
 }
